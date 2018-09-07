6cdce53634cb68ea6f6cb5dd1eeda2a5b22ed5dc
hymesh <hoyong.jung@meshkorea.net>
2018-09-07 19:22:54 +0900

    PRA-101_add-routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,6 +13,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/products', 'ProductController@index');
+
+Route::get('/products/{product}', 'ProductController@show');
+
+Route::get('/products/{product}/reviews', 'ProductController@showReviews');
+
+Route::group(['middleware' => 'auth.basic'], function () {
+    Route::post('/products', 'ProductController@store');
+
+    Route::put('/products/{product}', 'ProductController@update');
+
+    Route::delete('/products/{product}', 'ProductController@destroy');
+
+    Route::post('/products/{product}/reviews', 'ProductController@storeReview');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
